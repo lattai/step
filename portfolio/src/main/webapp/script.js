24a8979231b2df0946d943fa169732005067ce73
leahatt <lattai>
2020-05-26 15:34:37 -0400

    Added a flexbox for the subparagraphs (About me, Past projects, and Links) and created functions that apply to the About Me onClick and onHover. On Hover, a message is sent  to the console, notifying me that the entire div is applied onHover. The onClick function adds text below the flexbox.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -27,3 +27,12 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function hover(){
+    console.log("Hovered")
+}
+function expand(){
+    console.log("About me clicked")
+    const aboutMeContainer = document.getElementById('about-container');
+    aboutMeContainer.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+}
