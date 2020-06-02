29c14bf03d2088f8164073cc70f7aa08b3a5d7b4
leahatt <lattai>
2020-06-02 14:02:00 +0000

    Create list of items in index.html from Arraylist in DataServlet.java using JSON

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+    private  ArrayList<String> messages;
+
+    @Override
+    public void init(){
+    messages = new ArrayList<>();
+    messages.add("Howdy! Howdy! Howdy!");
+    messages.add("Purple Puppy Chow");
+    messages.add("Shrimp! Heaven! Now!");
+    }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Leah!</h1>");
+    // Convert ArrayList to JSON
+    response.setContentType("application/json;");
+    response.getWriter().println(convertToJsonWithGSon(messages));
   }
+  private String convertToJsonWithGSon(ArrayList messages) {
+      Gson gson = new Gson();
+      String json = gson.toJson(messages);
+      return json;
+  }
+  
 }
